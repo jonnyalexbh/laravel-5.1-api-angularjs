00bfe3fdcee627c05ae3bb8d63c9ff964b15eb1b
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-03-22 10:58:08 -0500

    create notes

--- angularClient/scripts/controllers/note.js
+++ angularClient/scripts/controllers/note.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('myApp')
-.controller('IndexNoteCtrl', function () {
 
+.controller('IndexNoteCtrl', function () {
+})
+
+.controller('CreateNoteCtrl', function($scope, NoteResource){
+  $scope.title = "Create Note";
+  $scope.button = "Save";
+  $scope.Note = {};
+  $scope.saveNote = function(){
+    //console.log($scope.Note);
+    NoteResource.save($scope.Note);
+  };
 });
---
+++ angularClient/scripts/services/NoteService.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('myApp')
+.factory('NoteResource',function($resource){
+  return $resource("http://localhost:8000/notes/:id",{id: "@id"});
+});
